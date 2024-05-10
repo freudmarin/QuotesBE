b7982a3f46908fdddf2462cde7abf67c0a4805d2
Marin Dulja <freudmarin@gmail.com>
2024-05-10 10:37:05 +0200

    first commit

---
+++ QuotesDashboardBackend/src/main/java/com/marin/quotesdashboardbackend/QuotesDashboardBackendApplication.java
@@ -0,0 +1,13 @@
+package com.marin.quotesdashboardbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuotesDashboardBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuotesDashboardBackendApplication.class, args);
+    }
+
+}
---
+++ QuotesDashboardBackend/src/test/java/com/marin/quotesdashboardbackend/QuotesDashboardBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marin.quotesdashboardbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuotesDashboardBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
